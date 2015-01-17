4f5a17212046cdfc4cf517fa28ec063f07990010
'yanche' <'belongs@live.cn'>
2015-01-17 20:30:49 +0800

    admin test

---
+++ fitu/clients/adminwebclient/app/js/config.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    angular.module('fitu_ad', ['ui.router', 'ui.router.stateHelper', 'ngRoute', 'fitulib', 'fituhtml'])
+    .config(['$stateProvider', 'stateHelperProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider',
+        function ($stateProvider, stateHelperProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
+        $httpProvider.defaults.withCredentials = true;
+        $locationProvider.html5Mode(true);
+        stateHelperProvider.setNestedState({
+            name: 'main',
+            templateUrl: '/app/html/main.html',
+            controller: 'main',
+            url: '/main'
+        });
+        $urlRouterProvider.when('/', '/main');
+    }]);
+})();
---
+++ fitu/clients/adminwebclient/app/js/controllers/main.js
@@ -0,0 +1,6 @@
+﻿(function () {
+    angular.module('fitu_ad')
+    .controller('main', ['$scope', '$window', function ($scope, $window) {
+        $scope.word = 'test word';
+    }]);
+})();
