8e38875195d8d4e4db04f57ce1a59a421456d2b8
'yanche' <'belongs@live.cn'>
2015-01-20 23:24:56 +0800

    dropdown

--- fitu/clients/userwebclient/app/js/controllers/activities/activities.js
+++ fitu/clients/userwebclient/app/js/controllers/activities/activities.js
@@ -1,46 +1,51 @@
 ﻿(function () {
     angular.module('fitu')
-    .controller('activities', ['$scope', '$rootScope', '$state', 'activity', 'ucconst', 'pagination', '$location', function ($scope, $rootScope, $state, activity, ucconst, pagination, $location) {
-        var ctx = $location.search();
-        
-        var pageStore = new pagination.PageStore(function (page, pageSize) {
-            return activity.getList({ page: page, pageSize: pageSize, tag: ctx.tag, siteId: ctx.siteId, vendorId: ctx.vendorId });
-        });
-        
-        var pageSize = 5;
-        $scope.visibles = [];
-        $scope.loading = false;
-        $scope.currentPage = 0;
-        //caution!! multi-request
-        $scope.switchPage = function (page) {
-            $scope.loading = true;
-            pageStore.navigate(page, pageSize)
-            .then(function (list) {
-                $scope.loading = false;
-                $scope.visibles = list;
-                $scope.currentPage = page;
-            })
-            .catch(function (err) {
-                $scope.loading = false;
-                console.log(err);
+    .controller('activities', ['$scope', '$rootScope', '$state', 'activity', 'ucconst', 'pagination', '$location', 'const', function ($scope, $rootScope, $state, activity, ucconst, pagination, $location, constants) {
+            var ctx = $location.search();
+            
+            var pageStore = new pagination.PageStore(function (page, pageSize) {
+                return activity.getList({ page: page, pageSize: pageSize, tag: ctx.tag, siteId: ctx.siteId, vendorId: ctx.vendorId });
             });
-        };
-        $scope.switchPage(0);
-        
-        $scope.getPageNavs = function () {
-            return pageStore.getPageNavs(pageSize, 3, $scope.currentPage);
-        };
-        
-        $scope.goDetail = function (actId) {
-            $state.gox(ucconst.states.actdetail, { actId: actId });
-        };
-        
-        $scope.tag = ctx.tag;
-        $scope.switchTag = function () {
-            if ($scope.tag)
-                $state.gox(ucconst.states.activities, { tag: $scope.tag });
-            else
-                $state.gox(ucconst.states.activities);
-        };
-    }]);
+            
+            var pageSize = 5;
+            $scope.visibles = [];
+            $scope.loading = false;
+            $scope.currentPage = 0;
+            //caution!! multi-request
+            $scope.switchPage = function (page) {
+                $scope.loading = true;
+                pageStore.navigate(page, pageSize)
+            .then(function (list) {
+                    $scope.loading = false;
+                    $scope.visibles = list;
+                    $scope.currentPage = page;
+                })
+            .catch(function (err) {
+                    $scope.loading = false;
+                    console.log(err);
+                });
+            };
+            $scope.switchPage(0);
+            
+            $scope.getPageNavs = function () {
+                return pageStore.getPageNavs(pageSize, 3, $scope.currentPage);
+            };
+            
+            $scope.goDetail = function (actId) {
+                $state.gox(ucconst.states.actdetail, { actId: actId });
+            };
+            
+            $scope.tags = Array.prototype.concat([], constants.tags);
+            var defaultTag = { value: '所有类型' };
+            $scope.currentTag = $scope.tags.filter(function (tag) { return tag.key == ctx.tag; })[0] || defaultTag;
+            $scope.tags.unshift(defaultTag);
+            $scope.switchTag = function () {
+                if ($scope.currentTag) {
+                    if ($scope.currentTag.key)
+                        $state.gox(ucconst.states.activities, { tag: $scope.currentTag.key });
+                    else
+                        $state.gox(ucconst.states.activities);
+                }
+            };
+        }]);
 })();
---
+++ fitu/clients/webclientshared/app/js/directives/dropdown.js
@@ -0,0 +1,31 @@
+﻿(function () {
+    angular.module('fitulib')
+    .directive('dropdown', ['$timeout', function ($timeout) {
+        return {
+            restrict: 'E',
+            templateUrl: '/app/html/directives/dropdown.html',
+            replace: true,
+            scope: {
+                items: '=',
+                selectedItem: '=',
+                change: '&change',
+                up: '=',
+                labelName: '='
+            },
+            link: function (scope, element, attrs) {
+                scope.select = function (item) {
+                    scope.expand = false;
+                    if (item != scope.selectedItem) {
+                        scope.selectedItem = item;
+                        $timeout(scope.change, 10);
+                    }
+                };
+                    
+                element.on('blur', function () {
+                    scope.expand = false;
+                    scope.$apply();
+                });
+            }
+        };
+    }]);
+})();
