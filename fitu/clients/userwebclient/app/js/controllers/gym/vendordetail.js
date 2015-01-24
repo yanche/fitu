63aec48148c88f282778fb739874cd90751faab2
'yanche' <'belongs@live.cn'>
2015-01-24 16:14:33 +0800

    patch

--- fitu/clients/userwebclient/app/js/controllers/gym/vendordetail.js
+++ fitu/clients/userwebclient/app/js/controllers/gym/vendordetail.js
@@ -2,11 +2,14 @@
     angular.module('fitu')
     .controller('vendordetail', ['$scope', 'vendor', '$location', function ($scope, vendor, $location) {
         var ctx = $location.search();
+        $scope.loading = true;
         vendor.getOne({ id: ctx.vendorId })
         .then(function (data) {
             $scope.vendor = data;
+            $scope.loading = false;
         }, function (err) {
             console.log(err);
+            $scope.loading = false;
         });
     }]);
 })();
