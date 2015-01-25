89b693c0f8966909ea430be051ce3373b220f2ff
'yanche' <'belongs@live.cn'>
2015-01-25 23:07:06 +0800

    time picker, current date bug

--- fitu/clients/webclientshared/app/js/directives/datetimePicker.js
+++ fitu/clients/webclientshared/app/js/directives/datetimePicker.js
@@ -3,10 +3,20 @@
     .directive('datetimePicker', ['$rootScope', function ($rootScope) {
         return {
             restrict: 'A',
+            scope: { 'ngModel': '=' },
             link: function (scope, element, attrs) {
                 element.datetimepicker({
                     format: $rootScope.const.dateTimeFormat,
                     minuteStepping: 5
+                });
+                    
+                var dp = element.data('DateTimePicker');
+                    
+                scope.$watch('ngModel', function (newVal, oldVal) {
+                    if (newVal && dp.date.format($rootScope.const.dateTimeFormat) != newVal) {
+                        //console.log(newVal);
+                        dp.setDate(newVal);
+                    }
                 });
             }
         };
