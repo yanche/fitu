4f372d1df8a44a717f109ed82a07ffb490ebfc13
'yanche' <'belongs@live.cn'>
2015-03-17 01:07:01 +0800

    img test

--- fitu/clients/webclientshared/app/js/directives/imageInput.js
+++ fitu/clients/webclientshared/app/js/directives/imageInput.js
@@ -5,7 +5,7 @@
             restrict: 'E',
             templateUrl: '/app/html/directives/imageInput.html',
             replace: true,
-            link: function (scope, element, attrs) {
+                link: function (scope, element, attrs) {
                 scope.head = 'head' in attrs;
                 var h = Number(attrs.imageHeight), w = Number(attrs.imageWidth);
                 var modelGet = $parse(attrs.imageBind);
@@ -13,29 +13,34 @@
                     
                 var fileinputBtn = element.find('input[type=file]');
                 fileinputBtn.bind('change', function () {
-                    var file = fileinputBtn[0].files[0], fileReader = new FileReader();
-                    fileReader.readAsDataURL(file);
-                    fileReader.onload = function (e) {
-                        if (!isNaN(h) && !isNaN(w)) {
-                            var img = new Image();
-                            img.src = e.target.result;
-                            img.onload = function () {
-                                var canvas = document.createElement('canvas');
-                                canvas.width = w;
-                                canvas.height = h;
-                                var ctx = canvas.getContext("2d");
-                                ctx.drawImage(this, 0, 0, w, h);
-                                var dataURL = canvas.toDataURL("image/jpeg");
-                                //alert('resized, dataurl: ' + dataURL.slice(0, 20) + ', length: ' + dataURL.length + ', w,h:' + w + ' ' + h);
-                                modelSet(scope, dataURL);
+                    if (!FileReader) {
+                        alert('浏览器不支持图片上传');
+                    }
+                    else {
+                        var file = fileinputBtn[0].files[0], fileReader = new FileReader();
+                        fileReader.readAsDataURL(file);
+                        fileReader.onload = function (e) {
+                            if (!isNaN(h) && !isNaN(w)) {
+                                var img = new Image();
+                                img.src = e.target.result;
+                                img.onload = function () {
+                                    var canvas = document.createElement('canvas');
+                                    canvas.width = w;
+                                    canvas.height = h;
+                                    var ctx = canvas.getContext("2d");
+                                    ctx.drawImage(this, 0, 0, w, h);
+                                    var dataURL = canvas.toDataURL("image/jpeg");
+                                    //alert('resized, dataurl: ' + dataURL.slice(0, 20) + ', length: ' + dataURL.length + ', w,h:' + w + ' ' + h);
+                                    modelSet(scope, dataURL);
+                                    scope.$apply();
+                                };
+                            }
+                            else {
+                                modelSet(scope, e.target.result);
                                 scope.$apply();
-                            };
-                        }
-                        else {
-                            modelSet(scope, e.target.result);
-                            scope.$apply();
-                        }
-                    };
+                            }
+                        };
+                    }
                 });
             }
         };
