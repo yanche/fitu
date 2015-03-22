c93fbd5f078446dc6cdc70eee3cef2519f882eef
'Yan <'belongscy@163.com'>
2015-03-22 15:59:58 +0800

    show multi tags for site

--- fitu/clients/webclientshared/app/js/filters/tag.js
+++
@@ -1,9 +0,0 @@
-﻿(function () {
-    angular.module('fitulib')
-    .filter('tag', ['const', function (constants) {
-        return function (input) {
-            var match = constants.tags.filter(function (t) { return t.key === input; });
-            return match.length > 0 ? match[0].value : '';
-        };
-    }]);
-})();
---
+++ fitu/clients/webclientshared/app/js/filters/tags.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    angular.module('fitulib')
+    .filter('tags', ['const', function (constants) {
+        return function (input) {
+            if (Array.isArray(input)) {
+                var ret = [];
+                input.forEach(function (i) {
+                    var match = constants.tags.filter(function (t) { return t.key === i; });
+                    if (match.length > 0)
+                        ret.push(match[0].value);
+                });
+                return ret.join(', ');
+            }
+            else
+                return '';
+        };
+    }]);
+})();
